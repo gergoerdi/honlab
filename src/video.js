8c7d13c1451f207e2bc4ca29d7a5d6ca37bcd589
Gergo ERDI <gergo@erdi.hu>
2023-08-03 09:11:22 +0800

    Apparently this is more performant? sadface...

--- src/video.js
+++ src/video.js
@@ -6,20 +6,26 @@ crt.style.imageRendering = "pixelated";
 
 function render(vram) {
     let ptr = 0;
-    
+
     for (let row = 0; row < 32; ++row) {
         for (let subrow = 0; subrow < 8; ++subrow) {
             for (let col = 0; col < 64; ++col) {
                 const c = vram[row * 64 + col];
-                
+
                 let glyphLine = charset[subrow << 8 | c];
                 for (let j = 0; j < 8; ++j) {
                     let glyphPixel = glyphLine & 0x80;
-                    
-                    let [r, g, b] = glyphPixel ? [0xff, 0xff, 0xff] : [0x00, 0x00, 0x00];
-                    buf.data[ptr++] = r;
-                    buf.data[ptr++] = g;
-                    buf.data[ptr++] = b;
+
+                    if (glyphPixel) {
+                        buf.data[ptr++] = 0xff;
+                        buf.data[ptr++] = 0xff;
+                        buf.data[ptr++] = 0xff;
+                    } else {
+                        buf.data[ptr++] = 0x00;
+                        buf.data[ptr++] = 0x00;
+                        buf.data[ptr++] = 0x00;
+                    }
+
                     buf.data[ptr++] = 0xff;
 
                     glyphLine <<= 1;
