1eb366b446cc7c51e4b40719ceb14d3a4595136f
Gergo ERDI <gergo@erdi.hu>
2023-08-09 19:00:59 +0800

    Only close the tape selector dialog of the click is outside the dialog

--- src/ui/tape-filesel.js
+++ src/ui/tape-filesel.js
@@ -2,7 +2,17 @@ const tape_filesel = document.getElementById("tape-filesel");
 const tape_cards = document.getElementById("tape-cards");
 
 tape_filesel.addEventListener("click", (e) => {
-    if (e.target == tape_filesel)
+    if (e.target != tape_filesel) return;
+
+    const rect = e.target.getBoundingClientRect();
+    const clicked_in_dialog = (
+        rect.top <= e.clientY &&
+            e.clientY <= rect.top + rect.height &&
+            rect.left <= e.clientX &&
+            e.clientX <= rect.left + rect.width
+    );
+
+    if (!clicked_in_dialog)
         tape_filesel.close();
 });
 
