a7ac3d2264701e4d7b6db898e57cbda50c974b15
Gergo ERDI <gergo@erdi.hu>
2023-08-09 15:58:25 +0800

    Add download link to each tape image

--- src/ui/tape-filesel.js
+++ src/ui/tape-filesel.js
@@ -20,19 +20,27 @@ const mkTapeCard = (obj) => {
     const body = mkNode("div", "card-body");
     card.appendChild(body);
 
-    const title = mkNode("h5", "card-title", obj.title);
-    body.appendChild(title);
-
-    const text = mkNode("p", "card-text", obj.desc);
-    body.appendChild(text);
+    body.appendChild(mkNode("h5", "card-title", obj.title));
+    body.appendChild(mkNode("p", "card-text", obj.desc));
 
     const footer = mkNode("div", "card-footer");
     card.appendChild(footer);
-    const small = mkNode("small", "text-body-secondary", obj.footer);
-    footer.appendChild(small);
+    const footerdiv = mkNode("div", "d-flex");
+    footer.appendChild(footerdiv);
+    footerdiv.classList.add("justify-content-between");
+    footerdiv.appendChild(mkNode("small", "text-body-secondary", obj.footer));
 
-    // tape_cards.appendChild(card);
+    const linksmall = mkNode("small", "text-body-secondary");
+    footerdiv.appendChild(linksmall);
 
+    const link = mkNode("a", "card-link", "Download");
+    linksmall.appendChild(link);
+    link.href = obj.filename;
+    link.download = obj.filename.substring(obj.filename.lastIndexOf('/')+1);
+
+    link.addEventListener("click", (event) => {
+        event.stopPropagation();
+    });
 
     return card;
 };
@@ -41,19 +49,15 @@ const loadTapeCards = async (url) => {
     const response = await fetch(url);
     const tapes_obj = await response.json();
     for (const obj of tapes_obj.tapes) {
+        obj.filename = "image/hl2/" + obj.filename;
+
         const card = mkTapeCard(obj);
         tape_cards.appendChild(card);
-    card.addEventListener("click", () => {
-        tape_filesel.close("image/hl2/" + obj.filename);
-    });
+
+        card.addEventListener("click", () => {
+            tape_filesel.close(obj.filename);
+        });
     };
 };
 
 loadTapeCards("image/hl2/images.json");
-
-// addTapeCard({
-//     "filename": "image/hl2/takopt.mp3",
-//     "title": "TakOpt",
-//     "desc": "Takarmány-optimalizáló segédprogram",
-//     "footer": "1:59"
-// });
