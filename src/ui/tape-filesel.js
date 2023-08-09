3edb2412539df05e0f7b19122cf7b8a809d4f645
Gergo ERDI <gergo@erdi.hu>
2023-08-09 16:08:20 +0800

    refactor tape card creation

--- src/ui/tape-filesel.js
+++ src/ui/tape-filesel.js
@@ -6,7 +6,7 @@ tape_filesel.addEventListener("click", (e) => {
         tape_filesel.close();
 });
 
-const mkTapeCard = (obj) => {
+const createCard = (title, text, footer) => {
     const mkNode = (tag, cls, s) => {
         const node = document.createElement(tag);
         node.classList.add(cls);
@@ -20,29 +20,45 @@ const mkTapeCard = (obj) => {
     const body = mkNode("div", "card-body");
     card.appendChild(body);
 
-    body.appendChild(mkNode("h5", "card-title", obj.title));
-    body.appendChild(mkNode("p", "card-text", obj.desc));
+    body.appendChild(mkNode("h5", "card-title", title));
+    body.appendChild(mkNode("p", "card-text", text));
 
-    const footer = mkNode("div", "card-footer");
-    card.appendChild(footer);
-    const footerdiv = mkNode("div", "d-flex");
-    footer.appendChild(footerdiv);
-    footerdiv.classList.add("justify-content-between");
-    footerdiv.appendChild(mkNode("small", "text-body-secondary", obj.footer));
+    if (footer) {
+        const footer_div = mkNode("div", "card-footer");
+        card.appendChild(footer_div);
+        footer_div.appendChild(footer);
+    }
+
+    return card;
+}
+
+const mkTapeCard = (obj) => {
+    const mkNode = (tag, cls, s) => {
+        const node = document.createElement(tag);
+        node.classList.add(cls);
+        if (s)
+            node.appendChild(document.createTextNode(s));
+        return node;
+    };
+
+    const footer = mkNode("div", "d-flex");
+    footer.classList.add("justify-content-between");
+    footer.appendChild(mkNode("small", "text-body-secondary", obj.footer));
 
     const linksmall = mkNode("small", "text-body-secondary");
-    footerdiv.appendChild(linksmall);
+    footer.appendChild(linksmall);
 
     const link = mkNode("a", "card-link", "Download");
     linksmall.appendChild(link);
-    link.href = obj.filename;
-    link.download = obj.filename.substring(obj.filename.lastIndexOf('/')+1);
+    link.href = "#";
 
     link.addEventListener("click", (event) => {
         event.stopPropagation();
     });
 
-    return card;
+    const card = createCard(obj.title, obj.desc, footer);
+
+    return { card, link };
 };
 
 const loadTapeCards = async (url) => {
@@ -51,8 +67,13 @@ const loadTapeCards = async (url) => {
     for (const obj of tapes_obj.tapes) {
         obj.filename = "image/hl2/" + obj.filename;
 
-        const card = mkTapeCard(obj);
+        const card_link = mkTapeCard(obj);
+        const card = card_link.card;
+        const link = card_link.link;
         tape_cards.appendChild(card);
+
+        link.href = obj.filename;
+        link.download = obj.filename.substring(obj.filename.lastIndexOf('/')+1);
 
         card.addEventListener("click", () => {
             tape_filesel.close(obj.filename);
@@ -61,3 +82,8 @@ const loadTapeCards = async (url) => {
 };
 
 loadTapeCards("image/hl2/images.json");
+// (() => {
+//     const footer = null;
+//     const card = createCard("New tape", "", footer);
+//     tape_cards.appendChild(card);
+// })();
